a18a261cf5dbe6836cf24d6aedf2d18e4769718e
sophiewhittle <sophiewhittle89@gmail.com>
2022-02-03 09:30:16 -0500

    udpate db to Postgres

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///to-do-list.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
