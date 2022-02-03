4f59a6fd120dd61c94c29be314e9d54392a65063
sophiewhittle <sophiewhittle89@gmail.com>
2022-02-03 10:21:20 -0500

    update postgres to postgresql v5

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ uri = os.environ.get('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///to-do-list.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
