6b93809f9fb20d25c69c275036f67eb527fa8052
sophiewhittle <sophiewhittle89@gmail.com>
2022-02-03 09:52:52 -0500

    update postgres to postgresql v2

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ app = Flask(__name__)
 
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
+uri = os.environ.get('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
